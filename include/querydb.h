c31a4bd8f439600a2d93921edf368cb9dd980141
changephilip <pcchange90@outlook.com>
2021-03-08 15:41:16 +0800

    update

---
+++ include/querydb.h
@@ -0,0 +1,14 @@
+#include "../include/readR2.h"
+
+class rQuery{
+ public:
+        rQuery(const rFirst& r1, const rSecond & r2, const loadedDB & lddb);
+        std::vector<uint32_t> rnaQ;
+};
+
+rQuery::rQuery(const rFirst &r1,const rSecond &r2, const loadedDB & lddb ){
+        rnaQ.resize(lddb.)
+        for (rSecondRead r2read:r2.reads){
+
+        }
+}
